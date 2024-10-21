3ee688e8b9fa1e461862500a0ff271e56fcbc62c
Issercio <didi_1374@outlook.fr>
2024-10-21 09:33:43 +0000

    C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific string to the standard output
+ * using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
