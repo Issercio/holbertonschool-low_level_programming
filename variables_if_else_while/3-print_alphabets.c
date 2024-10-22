2e4afc584033e8f90e0a5b71efc88bf7b3571181
root <root@Didi.localdomain>
2024-10-22 09:24:35 +0200

    C

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main Entry point
+ *
+ * Description: Prints the alphabet in lowercase and uppercase
+ * using only the putchar fonction
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar (letter);
+}
+putchar ('\n')
+for (letter = 'A'; letter <='Z'; letter++)
+{
+putchar (letter);
+}
+putchar ('\n')
+
+return 0;
+}
