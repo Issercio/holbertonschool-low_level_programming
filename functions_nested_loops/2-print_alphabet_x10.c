d14b5881f447a7856ec18b100a93822005495dfc
root <root@Didi.localdomain>
2024-10-22 13:14:10 +0200

    C

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_alphabet - Prints the alphabet x10 times in lowercase
+ * print_alphabet - Prints the alphabet 10 times
  *
- * Description: Prints x10 the alphabet in lowercase
- *
+ * Description: This function prints all lowercase letters
+ * of the English alphabet from 'a' to 'z', ten times
+ * Each set of the alphabet is followed by a new line.
  * Return: void
  */
 void print_alphabet_x10(void)
