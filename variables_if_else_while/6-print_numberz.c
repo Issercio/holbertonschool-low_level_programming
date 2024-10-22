be34558ac28b5c509be5b4aadb3c57756a3d6390
root <root@Didi.localdomain>
2024-10-22 10:13:33 +0200

    C

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers
+ * base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numbers;
+for (numbers = '0'; numbers <= '9'; numbers++)
+{
+putchar (numbers);
+}
+{
+putchar ('\n');
+}
+
+return (0);
+}
