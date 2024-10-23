a3d2202bcae5d61be3d8fdd6d4f0aa7dd61cf887
root <root@Didi.localdomain>
2024-10-23 09:43:48 +0200

    C

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,8 +5,7 @@
  * _islower - check for the lowercase
  * @c: the charcter to be checked
  *
- * Return: 1 if c is lowercase, 
- * 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
