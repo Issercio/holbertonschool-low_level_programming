08a74ed26214ff82fc70dbd28ede984f6ee49255
root <root@Didi.localdomain>
2024-10-23 11:19:48 +0200

    C

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Check that function that computes the absolute value
+ * @n: the integer to compute the absolute value of
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int)
+{
+if (n < 0)
+return (-n);
+return (n);
+}
