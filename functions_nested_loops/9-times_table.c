7f5926d4c593016d2877a9f84e8b11565fda877f
root <root@Didi.localdomain>
2024-10-23 12:22:19 +0200

    C

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_tables - Prints the 9 table
+ *
+ * Description: Prints the 9 table starting with 0
+ *
+ *
+ */
+void times_tables(void)
+{
+int i, j, result;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+result = 1* j;
+if (j ==0)
+{
+_putchar('0');
+}
+else if (result < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(result + '0');
+}
+else
+{
+_putchar('.');
+_putchar(' ');
+_putchar((result / 10) +'0');
+_putchar((result % 10) +'0');
+}
+}
+_putchar('\n');
+}
+}
