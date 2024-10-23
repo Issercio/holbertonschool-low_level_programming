96c76d1a2480fb16521ca9b2d4479bd984df207c
root <root@Didi.localdomain>
2024-10-23 09:32:50 +0200

    C

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check for the lowercase
+ * @c: the charcter to be checked
+ *
+ * Return 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
