68dd8025e716029438aa112e1eead1404c4e37a6
root <root@Didi.localdomain>
2024-10-23 09:39:08 +0200

    C

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,7 +5,8 @@
  * _islower - check for the lowercase
  * @c: the charcter to be checked
  *
- * Description: Check the input for the lowercase
+ * Description: Check if the input character
+ * is a lowercase letter (from 'a' to 'z').
  *
  * Return 1 if c is lowercase, 0 otherwise
  */
