5b933fb6a9febd8198cff14da876994c34f13e01
root <root@Didi.localdomain>
2024-10-23 10:03:06 +0200

    C

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - Check the function for alphabetic character
+ * @c: The character to be checked
+ *
+ * Description: Write a function that check alphabetic character
+ *
+ * Return: 1 if c is character, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
