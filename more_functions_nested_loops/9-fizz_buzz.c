6f6e2a053e3bf1bdf4ec0e2a4c2068a26e8cbe01
root <root@Didi.localdomain>
2024-10-28 15:23:35 +0100

    Loops

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,15 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * Fizz_Buzz - Print numbers from 1 to 100
+ * but for the multiplies of 3 print Fizz,for the multiplies of 5 print Buzz
  *
- * Return: Always 0 (Success)
  */
 void fizz_buzz(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+for (i = '1'; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
@@ -26,7 +27,7 @@ else
 {
 printf("%d", i);
 }
-if (i < 100)
+if (1 < 100)
 {
 printf(" ");
 }
