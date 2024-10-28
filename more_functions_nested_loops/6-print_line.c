1908c0ac09517fa2319478867783dfade815ddd3
root <root@Didi.localdomain>
2024-10-28 13:45:41 +0100

    Loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line.c - Check the function that draw a straight line
+ * @n: the function to be checked
+ *
+ * Description: Write a function that draw a straight line
+ *
+ * Return:n is 0, otherwise print \n
+ */
+void print_line(int n)
+{
+for (n = '0'; n <= '_';)
+{
+_putchar(n + '0');
+{
+_putchar('\n');
+}
+}
+}
+
