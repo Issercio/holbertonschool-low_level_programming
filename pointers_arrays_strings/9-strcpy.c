bbb5daf1f19a8c46bac6f807037ea1115e1a7313
root <root@Didi.localdomain>
2024-10-29 14:46:34 +0100

    Pointers

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Function
+ * @src: the string to copy from
+ *
+ * Description: Write a function that copies the string
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *start = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return start;
+}
