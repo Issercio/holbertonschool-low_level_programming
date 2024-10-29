6af74390bb58fe8e22866a909817762bd2dc7965
root <root@Didi.localdomain>
2024-10-29 14:25:34 +0100

    Pointers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,14 +12,14 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; < n; i++)
+for (i = 0; i < n; i++)
 {
 if (i > 0)
 {
 _putchar(',');
 _putchar(' ');
 }
-_putchar(a[i]);
+print_number(a[i]); 
 }
 _putchar('\n');
 }
