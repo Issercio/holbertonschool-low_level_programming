a8832d2750df89364b5aebef86c3b6044a50da7b
root <root@Didi.localdomain>
2024-10-29 17:13:17 +0100

    Pointers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -21,21 +21,17 @@ while (s[i] == ' ')
 {
 i++;
 }
-printf("After skipping spaces, current index: %d, character: '%c'\n", i, s[i]);
 while (s[i] == '-' || s[i] == '+')
 {
 if (s[i] == '-')
 sign *= -1;
-printf("Found sign: '%c', current sign: %d\n", s[i], sign);
 i++;
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
 num = num * 10 + (s[i] - '0');
 found_number = 1;
-printf("Building number: %u, current character: '%c'\n", num, s[i]);
 i++;
 }
-printf("Final result: %d\n", found_number ? sign * num : 0);
- return (0);
+return (found_number ? sign *num : 0);
 }
