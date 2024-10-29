8351afe4ecdcc8194c65524a04546c22ae0fc3c1
root <root@Didi.localdomain>
2024-10-29 12:32:46 +0100

    Pointers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - Function
+ * @n: the character to be checked
+ *
+ * Description: write a function that print half of a string
+ *
+ */
+void puts_half(char *str)
+{
+int length = 0;
+while (*str != '\0')
+{
+length++;
+str++;
+}
+}
