d4b1c60a0000e6c900a40156938dc4989f5d570d
root <root@Didi.localdomain>
2024-10-30 09:23:47 +0100

    Pointers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -22,13 +22,14 @@ sign *= -1;
 else if (*s >= '0' && *s <= '9')
 {
 digit_found = 1;
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && *s - '0' > INT_MAX % 10))
-return (sign == 1) ? INT_MAX : INT_MIN;
+if (result > INT_MAX / 10 ||
+(result == INT_MAX / 10 && *s - '0' > INT_MAX % 10))
+return ((sign == 1) ? INT_MAX : INT_MIN);
 result = result * 10 + (*s - '0');
 }
 else if (digit_found)
 break;
 s++;
 }
-return result * sign;
+return (result *sign);
 }
