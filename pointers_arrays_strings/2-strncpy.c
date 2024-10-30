94dc9676ab350c01e84488e2aa7a20c0bf6f1021
root <root@Didi.localdomain>
2024-10-30 12:40:15 +0100

    More Pointers

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,25 +5,21 @@
  * _strncpy - Function
  * @src: the string to copy from
  * @dest: the destination buffer where the string will be copied
- *
+ * @n: the number of bytes
  * Description: write a function that copies a string
  *
  * Return: the result of string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  char *start = dest;
-  int roger = 0;
-  while (*start != '\0')
-    {
-      start++;
-    }
-  while (*src != '\0' && roger < n) 
-    {
-    *dest = *src;
-      dest++;
-      src++;
-    }
-  *start = '\0';
-  return (dest);
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
