498af38c80aa66d6afb36f5c34e41a65c6994537
root <root@Didi.localdomain>
2024-10-31 10:35:13 +0100

    More Pointers

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,20 +11,25 @@
 char *cap_string(char *str)
 {
 int index = 0;
-if (str[index] >= 'a' && str[index] <= 'z')
-str -= 32;
-while (str[index])
+int capitalize_next = 1;
+while (str[index] != '\0')
 {
-if (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' ||
+if (capitalize_next && (str[index] >= 'a' && str[index] <= 'z'))
+{
+str[index] -= 32;
+capitalize_next = 0;
+}
+else if (str[index] == ' ' || str[index] == '\t' || str[index] == '\n' ||
 str[index] == ',' || str[index] == ';' || str[index] == '.' ||
 str[index] == '!' || str[index] == '?' || str[index] == '"' ||
 str[index] == '(' || str[index] == ')' || str[index] == '{' ||
 str[index] == '}')
 {
-if (str[index + 1] >= 'a' && str[index + 1] <= 'z')
-{
-str[index + 1] -= 32;
+capitalize_next = 1;
 }
+else
+{
+capitalize_next = 0;
 }
 index++;
 }
