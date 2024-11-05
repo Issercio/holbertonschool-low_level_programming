d00c7369e6b88360748e898d1db5339cce4f1adf
root <root@Didi.localdomain>
2024-11-05 11:50:34 +0100

    Recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _sqrt_recursion - function
+ * @n: if not a natural square return -1
+ *
+ * Description: write a function that return a natural square
+ *
+ * Return: 0 Always (Success)
+ */
+int _sqrt_recursion(int n)
+{
+  
