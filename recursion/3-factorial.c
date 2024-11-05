9f85315688c7fa1e6f70485d83a9f2c3cb5b419a
root <root@Didi.localdomain>
2024-11-05 10:20:23 +0100

    Recusrion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * factorial - function
+ * @n:lower than 0 = -1
+ *
+ * Description: write a function that return a factorial
+ *
+ * Return: 0 Always (Success)
+ */
+int factorial(int n)
+{
