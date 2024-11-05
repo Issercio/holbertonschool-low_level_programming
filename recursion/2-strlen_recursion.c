614c353f1524e73f23efd6c96300e80beafa76cd
root <root@Didi.localdomain>
2024-11-05 09:46:58 +0100

    Recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _strlen_recusrion - function
+ *
+ * Description: write a function that return a length of a string
+ *
+ * Return: 0 Always (Success)
+ */
+int _strlen_recursion(char *s)
+{
