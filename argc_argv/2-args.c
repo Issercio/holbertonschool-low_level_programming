7c3b1b0f3a83603b799ed62ca8de302852049b42
root <root@Didi.localdomain>
2024-11-07 10:43:13 +0100

    argc, argv

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,4 +2,19 @@
 
 /**
  * main - Entry point of the programm
- * @argc: 
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments strings
+ *
+ * Description: use the argument argc, argv to make a programm
+ *
+ * Return: 0 Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+int roger;
+for (roger = 0, roger < argc; roger++)
+{
+printf("%s\n", argv[1]);
+}
+return (0);
+}
