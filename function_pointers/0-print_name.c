1b5c25b0bd10fc30ddf2d9f7736a50cb46d891f3
root <root@Didi.localdomain>
2024-11-19 09:32:05 +0100

    Function_Pointers

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name using a given function
@@ -10,8 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-{
+if (name && f)
 f(name);
-}
 }
